0e4344073a7072db5c7d40c15f0384d1fcc77a8d
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-10-12 18:52:33 +0300

    COmplete IO lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
+    our_file = open(f'{file_name}.txt', mode='w', encoding= 'utf-8' )
+    our_file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    our_file = open(f'{file_name}.txt', mode='a', encoding= 'utf-8' )
+    our_file.write(append_content)
     pass
 
 def read_file(file_name):
-    pass
+    our_file = open(f'{file_name}.txt', mode='r', encoding= 'utf-8' )
+    return our_file.read()
+    
